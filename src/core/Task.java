290e49b918e8ec558a21a03a4ed11c635cb4dcdf
Tyler Rubino <tylerrubino@Tylers-MacBook-Pro.local>
2024-10-20 12:54:00 -0400

    initial commit

---
+++ src/core/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private double arrivalTime;
+
+    public Task(double arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getArrivalTime() {
+        return arrivalTime;
+    }
+}
